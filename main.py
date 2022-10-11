b896146da444159e3d4a8061e5123dfbd1ddbbf6
julkernaine <63929416+julkernaine@users.noreply.github.com>
2022-10-12 00:36:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import decode_qr
 from tkinter import *
 window=Tk()
 
 window.title('Auto Image Editor')
 window.geometry("300x200")
-button_1 = Button(Tk(),text = "Decode QR Code")  
+button_1 = Button(Tk(),text = "Decode QR Code", command =decode_qr.dec_qr)  
 button_1.pack()
 
 window.mainloop()
