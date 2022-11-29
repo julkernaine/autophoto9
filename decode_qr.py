f052228812f3ae81ee216d8144b36af2a87cf6bf
julkernaine <63929416+julkernaine@users.noreply.github.com>
2022-11-29 21:30:09 +0600

    Update decode_qr.py

--- decode_qr.py
+++ decode_qr.py
@@ -2,6 +2,10 @@
 def dec_qr():
     from pyzbar.pyzbar import decode
     from PIL import Image
-    qr_decode = decode(Image.open('QR_image.png'))
-    print('The decoded QR is: ' + qr_decode[0].data.decode('ascii'))
+    qr_decode = decode(Image.open('QR_image.png')) #opening the qr code
+
+    decoded_text = qr_decode[0].data.decode('ascii')
+
+    #print('The decoded QR is: ' + qr_decode[0].data.decode('ascii'))#decoding the qr into a text
+    print('The decoded QR is: ' + decoded_text)
 dec_qr()
