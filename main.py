2b63ef1f6f0dbc281ecd842c3a1ae76ae73db321
julkernaine <63929416+julkernaine@users.noreply.github.com>
2022-11-29 23:05:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 
 from tkinter import *
-
+import cv2
 from pyzbar.pyzbar import decode
 from PIL import Image
-
+import make_qr
+from image_com import comp_img
+from convert_img import convert_img
+from face_detection import face_det
 root = Tk()
 root.title('Imagify')
 
@@ -14,7 +17,7 @@ from PIL import Image
 qr_decode = decode(Image.open('QR_image.png'))
 decoded_text = qr_decode[0].data.decode('ascii')
 
-frame=Frame(root, width=500, height=700, bg='white')
+frame=Frame(root, width=600, height=500, bg='white')
 
 
 
@@ -50,9 +53,14 @@ from tkinter.filedialog import askopenfilename
 myButton = Button(root, text = "Decode QR Code",command = myClick,padx= 30,pady=20)
 
 myButton1 = Button(root, text = "Select the Image",command = askopenfilename,padx= 30,pady=20)
-myButton3 = Button(root, text = "Make QR Image", command = openNewwWindow,padx= 30,pady=20)
-#from face_detection import imshow,waitKey
-#myButton4 = Button(root, text = "Detect Faces", command = imshow,padx= 30,pady=20)
+myButton3 = Button(root, text = "Make QR Image", command = make_qr,padx= 30,pady=20)
+myButton4 = Button(root, text = "Compress Image", command = comp_img,padx= 30,pady=20)
+myButton5 = Button(root, text = "Sketch Art", command = convert_img,padx= 30,pady=20)
+myButton6 = Button(root, text = "Detect Faces", command = face_det,padx= 30,pady=20)
+
+
+
+
 
 
 
@@ -62,7 +70,10 @@ myButton3 = Button(root, text = "Make QR Image", command = openNewwWindow,padx= 
 myButton.pack()
 myButton1.pack()
 myButton3.pack()
-#myButton4.pack()
+myButton4.pack()
+myButton5.pack()
+myButton6.pack()
+
 myLabel.pack()
 frame.pack()
 root.mainloop()
