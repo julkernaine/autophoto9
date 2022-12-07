92bfdf886117c1e5f4d2ce7cebd44110f23d9c9c
julkernaine <63929416+julkernaine@users.noreply.github.com>
2022-12-07 21:48:04 +0600

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from face_detection import face_det
 root = Tk()
 root.title('Imagify')
 
-myLabel = Label(root, text = "Welcome!")
+
 from pyzbar.pyzbar import decode
 from PIL import Image
 
@@ -31,10 +31,6 @@ def myClick1():
     myLabel1.pack()
     
 
-
-
-
-
 def openNewwWindow():
     newWindow = Toplevel(root)
     newWindow.title("Enter Your Text Here")
@@ -46,7 +42,7 @@ def openNewwWindow():
     import pyqrcode , png
     qr_text = pyqrcode.create(e.get())
     qr_text.png("QR_image.png", scale= 5)
-    myButtonnew = Button(newWindow, text = "Make QR Code",command = myClick1,padx= 30,pady=20)
+    myButtonnew = Button(newWindow, text = "Make QR Code",command = myClick1,height= 5, width=50)
     myButtonnew.pack()
 
 def openNewwWindow1():
@@ -56,19 +52,14 @@ def openNewwWindow1():
     Label(newWindow,text ="The QR code is : "+ decoded_text).pack()
 
 
-
 from tkinter.filedialog import askopenfilename
 
-
-
-myButton = Button(root, text = "Decode QR Code",command = openNewwWindow1,height= 5, width=50,pady=20)
+myButton = Button(root, text = "Decode QR Code",command = openNewwWindow1,height= 5, width=50)
 myButton1 = Button(root, text = "Select the Image",command = askopenfilename,height= 5, width=50)
 myButton3 = Button(root, text = "Make QR Image", command = openNewwWindow,height= 5, width=50)
 myButton4 = Button(root, text = "Compress Image", command = comp_img,height= 5, width=50)
 myButton5 = Button(root, text = "Sketch Art", command = convert_img,height= 5, width=50)
 myButton6 = Button(root, text = "Detect Faces", command = face_det,height= 5, width=50)
-
-
 
 myButton.pack()
 myButton1.pack()
@@ -77,7 +68,5 @@ myButton4.pack()
 myButton5.pack()
 myButton6.pack()
 
-
-myLabel.pack()
 frame.pack()
 root.mainloop()
